5cf439b6f6447e032923c5ccd972162a12d5f2ca
xinlian123 <lianxin9342@gmail.com>
2019-10-08 15:31:35 -0400

    new code

--- ProjProfile/quotes/urls.py
+++ ProjProfile/quotes/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about.html', views.about, name='about'),
+    path('earning.html', views.earning, name='earning')
 ]
